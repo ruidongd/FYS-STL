ecaf54fdc04cad2c0babad713a77678c629680a8
RuidongDai <steve19950115@outlook.com>
2016-04-28 21:41:26 -0700

    add declartion
    default constructor
    push_back
    destructor

--- src/fysSTL.hpp
+++ src/fysSTL.hpp
@@ -5,26 +5,17 @@
 #ifndef FYSSTL_HPP
 #define FYSSTL_HPP
 
-namespace fys{
+namespace fys {
 
-	// start on April 28 2016
-	template <typename T>
-	class vector{
+// start on April 28 2016
+template <typename T> class vector {
+public:
+  vector();
+  void push_back(const T *elemet);
+  ~vector();
+};
 
-	};
-
-	template <typename T>
-	class map{
-
-	};
-
-
-
-
-
-
-
-}
-
+// template <typename T> class map {};
+//}
 
 #endif
